fd209172268f399b55e6c10854fdd3b8a626408b
ProgrammerMartin <flataker.martin@gmail.com>
2026-04-26 13:37:40 +0200

    ingenting

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-import time
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 # kNN-modell, k = 5
 k = 5
@@ -26,7 +25,7 @@ features = df.drop(columns=["Genre"]).values
 labels = df["Genre"].values
 
 print("sklearn")
-from sklearn.preprocessing import StandardScaler
+
 
 scaler = StandardScaler()
 features = scaler.fit_transform(features)
@@ -36,8 +35,12 @@ test_index = 100
 test_features = features[test_index]
 test_label = labels[test_index]
 
-distances = np.linalg.norm(features-test_features,axis=1)
+features = np.delete(features, (test_index), axis=0)
+labels = np.delete(labels, (test_index), axis=0)
+
+distances = np.linalg.norm(features - test_features, axis=1)
+
 idx = np.argpartition(distances, k)[:k]
 
-print("true label:",test_label)
-print("guess:",labels[idx])
+print("true label:", test_label)
+print("guess:", labels[idx])
