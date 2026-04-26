1b7ac0c44f23290643d03d52f62320f6a8374dc9
ProgrammerMartin <flataker.martin@gmail.com>
2026-04-26 14:03:25 +0200

    ingenting

---
+++ 3knn.py
@@ -0,0 +1,3 @@
+# Design a k-NN classifier (k=5) for all ten genres using only four features
+# with at least three features being; spectral rolloff mean, mfcc 1 mean, spectral centroid mean or tempo. Motivate why you selected the particular
+# four features.
---
+++ 4classifier.py
@@ -0,0 +1,4 @@
+# Design a classifier for all ten genres that classifies the audio tracks, each
+# represented by a Track ID. You are allowed to use any classifier, as many
+# features as you like and all of the available data sets GenreClassData 5s.txt,
+# GenreClassData 10s.txt, and GenreClassData 30s.txt as input data.
