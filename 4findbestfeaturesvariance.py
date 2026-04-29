6ac67930fdd1c1036308e1a47d9581f97563bdf5
AasmundAngard <wefling@live.no>
2026-04-29 20:33:44 +0200

    some work on task 4

---
+++ 4findbestfeaturesvariance.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+
+
+
+# Programmet skal beregne mean for alle sjangre i alle 630 features, 
+# og så sjekke varians på tvers av sjangre for alle features.
+# Deretter sjekke den gjennomsnittlige variansen innen hver sjanger, for hver feature
+
+# Deretter velge ut features med høyest:
+# varians mellom sjangre / varians innen sjangre
+
+
+# Test alle features, finn beste.
+
+datatypes = ['zero_cross_rate_mean', 'zero_cross_rate_std', 'rmse_mean', 
+              'rmse_var', 'spectral_centroid_mean', 'spectral_centroid_var', 
+              'spectral_bandwidth_mean', 'spectral_bandwidth_var', 
+              'spectral_rolloff_mean', 'spectral_rolloff_var', 
+              'spectral_contrast_mean', 'spectral_contrast_var', 
+              'spectral_flatness_mean', 'spectral_flatness_var', 
+              'chroma_stft_1_mean', 'chroma_stft_2_mean', 'chroma_stft_3_mean', 
+              'chroma_stft_4_mean', 'chroma_stft_5_mean', 'chroma_stft_6_mean', 
+              'chroma_stft_7_mean', 'chroma_stft_8_mean', 'chroma_stft_9_mean', 
+              'chroma_stft_10_mean', 'chroma_stft_11_mean', 'chroma_stft_12_mean', 
+              'chroma_stft_1_std', 'chroma_stft_2_std', 'chroma_stft_3_std', 
+              'chroma_stft_4_std', 'chroma_stft_5_std', 'chroma_stft_6_std', 
+              'chroma_stft_7_std', 'chroma_stft_8_std', 'chroma_stft_9_std', 
+              'chroma_stft_10_std', 'chroma_stft_11_std', 'chroma_stft_12_std', 
+              'tempo', 'mfcc_1_mean', 'mfcc_2_mean', 'mfcc_3_mean', 'mfcc_4_mean', 
+              'mfcc_5_mean', 'mfcc_6_mean', 'mfcc_7_mean', 'mfcc_8_mean', 
+              'mfcc_9_mean', 'mfcc_10_mean', 'mfcc_11_mean', 'mfcc_12_mean', 
+              'mfcc_1_std', 'mfcc_2_std', 'mfcc_3_std', 'mfcc_4_std', 'mfcc_5_std', 
+              'mfcc_6_std', 'mfcc_7_std', 'mfcc_8_std', 'mfcc_9_std', 'mfcc_10_std', 
+              'mfcc_11_std', 'mfcc_12_std'
+              ]
+
+cols = [
+     
+]
+
+# Retrieve all features
+
+df = pd.read_csv("Music files/GenreClassData_30s.txt", sep="\t")
+features_30 = df[df["Type"]=="Train"][["Genre"]+datatypes]
+labels = df["Genre"].values
+df = pd.read_csv("Music files/GenreClassData_10s.txt", sep="\t")
+features_10 = df[df["Type"]=="Train"][["Genre"]+datatypes]
+df = pd.read_csv("Music files/GenreClassData_5s.txt", sep="\t")
+features_5 = df[df["Type"]=="Train"][["Genre"]+datatypes]
+
+
+features_10_1 = df.iloc[0::3].reset_index(drop=True)
+features_10_2 = df.iloc[1::3].reset_index(drop=True)
+features_10_3 = df.iloc[2::3].reset_index(drop=True)
+
+features_5_1 = df.iloc[0::6].reset_index(drop=True)
+features_5_2 = df.iloc[1::6].reset_index(drop=True)
+features_5_3 = df.iloc[2::6].reset_index(drop=True)
+features_5_4 = df.iloc[3::6].reset_index(drop=True)
+features_5_5 = df.iloc[4::6].reset_index(drop=True)
+features_5_6 = df.iloc[5::6].reset_index(drop=True)
+
+
+# Calculate the genre means for each feature
+genre_means_30 = features_30.groupby("Genre")[datatypes].mean()
+
+genre_means_10_1 = features_10_1.groupby("Genre")[datatypes].mean()
+genre_means_10_2 = features_10_2.groupby("Genre")[datatypes].mean()
+genre_means_10_3 = features_10_3.groupby("Genre")[datatypes].mean()
+
+genre_means_5_1 = features_5_1.groupby("Genre")[datatypes].mean()
+genre_means_5_2 = features_5_2.groupby("Genre")[datatypes].mean()
+genre_means_5_3 = features_5_3.groupby("Genre")[datatypes].mean()
+genre_means_5_4 = features_5_4.groupby("Genre")[datatypes].mean()
+genre_means_5_5 = features_5_5.groupby("Genre")[datatypes].mean()
+genre_means_5_6 = features_5_6.groupby("Genre")[datatypes].mean()
+
+# Calculate the variance in mean between genres for each feature
+variance_between_classes_30 = genre_means_30.var(axis=0)
+
+variance_between_classes_10_1 = genre_means_10_1.var(axis=0)
+variance_between_classes_10_2 = genre_means_10_2.var(axis=0)
+variance_between_classes_10_3 = genre_means_10_3.var(axis=0)
+
+variance_between_classes_5_1 = genre_means_5_1.var(axis=0)
+variance_between_classes_5_2 = genre_means_5_2.var(axis=0)
+variance_between_classes_5_3 = genre_means_5_3.var(axis=0)
+variance_between_classes_5_4 = genre_means_5_4.var(axis=0)
+variance_between_classes_5_5 = genre_means_5_5.var(axis=0)
+variance_between_classes_5_6 = genre_means_5_6.var(axis=0)
+
+
+# Calculate the average variance within each genre for each feature
+variance_within_class_30 = features_30.groupby("Genre")[datatypes].var().mean(axis=0)
+
+variance_within_class_10_1 = features_10_1.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_10_2 = features_10_2.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_10_3 = features_10_3.groupby("Genre")[datatypes].var().mean(axis=0)
+
+variance_within_class_5_1 = features_5_1.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_5_2 = features_5_2.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_5_3 = features_5_3.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_5_4 = features_5_4.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_5_5 = features_5_5.groupby("Genre")[datatypes].var().mean(axis=0)
+variance_within_class_5_6 = features_5_6.groupby("Genre")[datatypes].var().mean(axis=0)
+
+# Calculate the relation between: variance between genres / variance within genres
+variance_relation_30 = variance_between_classes_30 / variance_within_class_30
+
+variance_relation_10_1 = variance_between_classes_10_1 / variance_within_class_10_1
+variance_relation_10_2 = variance_between_classes_10_2 / variance_within_class_10_2
+variance_relation_10_3 = variance_between_classes_10_3 / variance_within_class_10_3
+
+variance_relation_5_1 = variance_between_classes_5_1 / variance_within_class_5_1
+variance_relation_5_2 = variance_between_classes_5_2 / variance_within_class_5_2
+variance_relation_5_3 = variance_between_classes_5_3 / variance_within_class_5_3
+variance_relation_5_4 = variance_between_classes_5_4 / variance_within_class_5_4
+variance_relation_5_5 = variance_between_classes_5_5 / variance_within_class_5_5
+variance_relation_5_6 = variance_between_classes_5_6 / variance_within_class_5_6
+
+
+# Find features with highest relation
+
+
---
+++ 4knnvarianceandfeatures.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
+from datautilities import readfromtabcsv, zscorenormalize
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+
+
+# kNN modell med en kombinasjon av variasjon mellom features mellom 5s klipp og features i 30s klipp
+
+def find_nearest(model_features,model_labels,test_features):
+
+    distances = np.linalg.norm(model_features - test_features, axis=1)
+    indexes_of_k_closest = np.argpartition(distances, k)[:k]
+    nearest_labels = model_labels[indexes_of_k_closest]
+    nearest_distances = distances[indexes_of_k_closest]
+
+
+    counts = Counter(nearest_labels)
+    max_count = max(counts.values())
+
+    candidates = [label for label, count in counts.items() if count == max_count]
+
+    if len(candidates) == 1:
+        majority_class = candidates[0]
+    else:
+        distance_sums = {
+            label: np.sum(nearest_distances[nearest_labels == label])
+            for label in candidates
+        }
+        majority_class = min(distance_sums, key=distance_sums.get)
+
+    return majority_class
+
+
+# kNN-modell
+k = 7
+
+cols = [
+     "spectral_flatness_var",
+     "spectral_contrast_var",
+     "mfcc_8_std",
+     "rmse_var",
+     "chroma_stft_4_mean",
+     "spectral_bandwidth_mean",
+     "mfcc_5_std",
+     "spectral_flatness_mean",
+     "spectral_rolloff_mean",
+     "spectral_contrast_mean",
+     "mfcc_3_std",
+     "rmse_mean",
+     "mfcc_6_mean",
+     "spectral_rolloff_var",
+     "spectral_flatness_var",
+     "mfcc_2_mean",
+     "spectral_contrast_var",
+]
+datatypes = ['zero_cross_rate_mean', 'zero_cross_rate_std', 'rmse_mean', 
+              'rmse_var', 'spectral_centroid_mean', 'spectral_centroid_var', 
+              'spectral_bandwidth_mean', 'spectral_bandwidth_var', 
+              'spectral_rolloff_mean', 'spectral_rolloff_var', 
+              'spectral_contrast_mean', 'spectral_contrast_var', 
+              'spectral_flatness_mean', 'spectral_flatness_var', 
+              'chroma_stft_1_mean', 'chroma_stft_2_mean', 'chroma_stft_3_mean', 
+              'chroma_stft_4_mean', 'chroma_stft_5_mean', 'chroma_stft_6_mean', 
+              'chroma_stft_7_mean', 'chroma_stft_8_mean', 'chroma_stft_9_mean', 
+              'chroma_stft_10_mean', 'chroma_stft_11_mean', 'chroma_stft_12_mean', 
+              'chroma_stft_1_std', 'chroma_stft_2_std', 'chroma_stft_3_std', 
+              'chroma_stft_4_std', 'chroma_stft_5_std', 'chroma_stft_6_std', 
+              'chroma_stft_7_std', 'chroma_stft_8_std', 'chroma_stft_9_std', 
+              'chroma_stft_10_std', 'chroma_stft_11_std', 'chroma_stft_12_std', 
+              'tempo', 'mfcc_1_mean', 'mfcc_2_mean', 'mfcc_3_mean', 'mfcc_4_mean', 
+              'mfcc_5_mean', 'mfcc_6_mean', 'mfcc_7_mean', 'mfcc_8_mean', 
+              'mfcc_9_mean', 'mfcc_10_mean', 'mfcc_11_mean', 'mfcc_12_mean', 
+              'mfcc_1_std', 'mfcc_2_std', 'mfcc_3_std', 'mfcc_4_std', 'mfcc_5_std', 
+              'mfcc_6_std', 'mfcc_7_std', 'mfcc_8_std', 'mfcc_9_std', 'mfcc_10_std', 
+              'mfcc_11_std', 'mfcc_12_std'
+              ]
+
+
+train_features,train_labels,test_features,test_labels = readfromtabcsv("Music files/GenreClassData_"+"5s"+".txt",cols)
+train_features_30,train_labels_30,test_features_30,test_labels_30 = readfromtabcsv("Music files/GenreClassData_"+"30s"+".txt",cols)
+
+#Z score normalize axes
+mean = np.mean(train_features_30,axis=0)
+std = np.sqrt(np.var(train_features_30,axis=0))
+train_features_30 = (train_features_30-mean)/std
+test_features_30 = (test_features_30-mean)/std
+
+#Z score normalize axes
+mean = np.mean(train_features,axis=0)
+std = np.sqrt(np.var(train_features,axis=0))
+train_features = (train_features-mean)/std
+test_features = (test_features-mean)/std
+
+genres = np.unique(train_labels)
+
+train_features = train_features.reshape(-1, 6, train_features.shape[1])
+test_features = test_features.reshape(-1, 6, test_features.shape[1])
+
+train_labels = train_labels.reshape(-1,6)[:, 0]
+test_labels = test_labels.reshape(-1,6)[:, 0]
+
+
+train_vars = np.var(train_features, axis=1)
+test_vars = np.var(test_features,axis=1)
+
+train_combined = np.concatenate([train_features_30, train_vars], axis=1)
+test_combined = np.concatenate([test_features_30, test_vars], axis=1)
+
+
+genre_to_index = {genres[i]: i for i in range(len(genres))}
+confusion_matrix = np.zeros((len(genres),len(genres)))
+correct_per_genre = {g: 0 for g in genres}
+total_per_genre = {g: 0 for g in genres}
+correctly_classified = 0
+
+correct = 0
+total = 0
+print(test_vars.shape,test_labels.shape)
+for test_var,true_label in zip(test_combined,test_labels):
+    
+    majority_class = find_nearest(train_combined,train_labels,test_var)
+    print(true_label,majority_class)
+    if majority_class == true_label:
+        correct += 1
+
+    confusion_matrix[genre_to_index[true_label]][genre_to_index[majority_class]]+= 1
+
+    total += 1
+    # correct_per_genre[true_label] += 1
+
+    # total_per_genre[true_label] += 1
+
+print(correct,"/",total)
+print(correct/total)
+
+
+
+
+
+
+
+# for test_feature,true_label in zip(test_features,test_labels):
+
+#     majority_class = find_nearest(train_features,train_labels,test_feature)
+    
+#     confusion_matrix[genre_to_index[true_label]][genre_to_index[majority_class]]+= 1
+
+#     if majority_class == true_label:
+#         correctly_classified += 1
+
+
+
+
+
+# treff_forhold = correctly_classified / len(test_features)
+# print(f"Treffprosent totalt: {100*treff_forhold:.2f}%")
+
+
+# print("\nTreffprosent per sjanger:")
+
+# for g in genres:
+#     if total_per_genre[g] > 0:
+#         acc = correct_per_genre[g] / total_per_genre[g]
+#         print(f"{g}: {100*acc:.2f}% ({correct_per_genre[g]}/{total_per_genre[g]})")
+
+
+
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,
+                            display_labels=genres,
+                            # cmap=plt.cm.Blues
+                            )
+# disp.plot()
+disp.plot(cmap="Blues")
+plt.xticks(rotation=45, ha="right")  # rotate x-axis labels
+plt.title("Confusion matrix kNN classification, k="+str(k))
+plt.tight_layout()
+plt.savefig("plots/selectfeatvar5feat30",bbox_inches="tight")
