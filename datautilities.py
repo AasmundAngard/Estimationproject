b24b2497a4825f23984c6b9c666b3f9d7fe182d1
AasmundAngard <wefling@live.no>
2026-04-30 23:48:08 +0200

    update files

--- datautilities.py
+++ datautilities.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-
 
 def readfromtabcsv(filepath,cols):
     df = pd.read_csv(filepath, sep="\t", usecols=cols+["Genre","Type"])
@@ -13,15 +11,4 @@ def readfromtabcsv(filepath,cols):
 
     return train_features,train_labels,test_features,test_labels
 
-def zscorenormalize(matrix,mean=None,std=None):
-    # Expects matrix on form (samples,features)
-    # vector = (vector-mu)/sigma
-    if mean!=None and std!=None:
-        normalized_matrix = (matrix-mean)/std
-    else:
-        mean = np.mean(matrix,axis=0)
-        std = np.sqrt(np.var(matrix,axis=0))
-        normalized_matrix = (matrix-mean)/std
-
-    return normalized_matrix, mean, std
 
